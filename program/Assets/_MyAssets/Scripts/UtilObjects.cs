6872d8e08c6ab2f6d9394a2609e9007239050a0d
mu3 <74529749+doqltl179@users.noreply.github.com>
2024-02-26 07:10:40 +0900

    ui 이미지 추가, 폰트 추가

--- program/Assets/_MyAssets/Scripts/UtilObjects.cs
+++ program/Assets/_MyAssets/Scripts/UtilObjects.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class UtilObjects : ResourceGenericSingleton<UtilObjects> {
     #region Camera
